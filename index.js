0f05c6b327ae021e800044a988967be1d6abb678
Raj Aayush <rajif2014ru@gmail.com>
2023-08-24 20:55:31 +0530

    cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const connect = async () => {
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/tours', tourRoute);
