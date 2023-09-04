f8937d46746cca1aab477ef4344e3822232b14ad
Raj Aayush <77530164+doraemon305@users.noreply.github.com>
2023-09-04 22:40:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/tours', tourRoute);
