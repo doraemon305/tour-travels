a82c2fb779604aff3e82070c06c75f9b645b9f44
Raj Aayush <rajif2014ru@gmail.com>
2023-09-24 20:31:53 +0530

    commited

--- routes/booking.js
+++ routes/booking.js
@@ -4,8 +4,8 @@ import { createBooking, getAllBooking, getBooking } from '../controllers/booking
 
 const router = express.Router();
 
-router.post('/', verifyUser, createBooking);
-router.get('/:id', verifyUser, getBooking);
+router.post('/', createBooking);
+router.get('/:id', getBooking);
 router.get('/', verifyAdmin, getAllBooking);
 
 export default router;
